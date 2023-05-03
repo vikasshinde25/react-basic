373315749e3d454245cc96c75fb8f13547284b62
Vikas <vikas.s@amazatic.com>
2023-05-03 21:51:20 +0530

    [study] :: demo commit 3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    console.log('--------------------');
     return (
         <div className="App">
             <header className="App-header">
